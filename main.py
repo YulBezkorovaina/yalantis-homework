9218da03cb6bb436520e14b0f6f17169d655737f
Yuliia Bezkorovaina <yuliia.bezkorovaina@npp.nau.edu.ua>
2022-01-13 13:26:37 +0200

    Second commint

---
+++ main.py
@@ -0,0 +1,21 @@
+ #!venv/bin/python
+
+if __name__ == "__main__":
+    print("Hello, I am program!")
+    a = 1
+    print(dir(a))
+
+    #methods STR
+    S = "Hello my teacher!"
+    print(S.isupper())
+    S1 = S.upper()
+    print(S1.isupper())
+    print(S)
+    print(S.find("my"))
+    print(S.split(" "))
+
+    #methods dict
+
+
+
+
