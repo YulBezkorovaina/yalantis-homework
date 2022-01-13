7ab9842a39d7115375e93ca9c2df5d0664da8e38
Yuliia Bezkorovaina <yuliia.bezkorovaina@npp.nau.edu.ua>
2022-01-13 16:24:15 +0200

    Commit lesson_1_taks branch

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     print(S.find("my"))
     print(S.split(" "))
 
-    #methods dict
+    #methods dict lesson_1_taks
     d : dict = {'d' : 1, 'b' : 2, 'a' : 3}
     print(d)
     print(d.get('a'))
     print(d['b'])
     print(d.keys())
 
-    #methods list
+    #methods list  lesson_1_taks
     l : list = ['abc', 'def', '123', '456', 'ad1', 'be4']
     print(l)
     l.pop(1)
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     l.reverse()
     print(l)
 
-    #methods set
+    #methods set  lesson_1
     s : set = set(S)
     print(s)
     print(len(s))
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     s2 : set = s.union(s1)
     print(s2)
 
-    #methods tuple
+    #methods tuple  lesson_1
     t : tuple = s2
     print(t)
     print(t.intersection('e'))
